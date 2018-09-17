dfdf96359a3633e37b50073c770c2c2b97eb01f8
mooncake4132 <mooncake4132@gmail.com>
2018-09-16 19:04:07 -0700

    initial

---
+++ sync_repos.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+
+DFVSYNC_PACKAGE=https://github.com/mooncake4132/dfvsync
+
+
+trigger_dfvsync() {
+    url=$1
+
+    tmpdir=$(mktemp -d)
+    git clone "$url" "$tmpdir"
+    pushd "$tmpdir"
+
+    pip install "$DFVSYNC_PACKAGE"
+    dfvsync
+}
+
+
+repos=$(cat .repos.txt)
+for url in $repos
+do
+    trigger_dfvsync "$url"
+done
